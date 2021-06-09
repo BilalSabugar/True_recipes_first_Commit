8cd0c56034294bc83922506ec077f7b0c9f1518d
Bilal <bilal.07.04.07@gmail.com>
2021-06-10 00:39:02 +0530

    WishList done sucessfully

---
+++ Category/NonVeg/MainScreen/localdb.js
@@ -0,0 +1,163 @@
+const db = [
+  {
+    "albumId": 1,
+    "id": 1,
+    "title": "Non Veg",
+    "url": require("../../../assets/VegScreenImages/WholeWheatPasta.png"),
+    "navigation": "Pasta_in_Mushroom_Sauce",
+    "wishlistColor":'white'
+  },
+  {
+    "albumId": 1,
+    "id": 2,
+    "title": "Aloo Tamatar Ka Jhol",
+    "url": require('../../../assets/VegScreenImages/Aloo-Tamatar-Ka-Jhol.png'),
+    "navigation": "Aloo_Tamatar_Ka_Jhol",
+    "wishlistColor":'white'
+  },
+  {
+    "albumId": 1,
+    "id": 3,
+    "title": "Tamarind Rice",
+    "url": require('../../../assets/VegScreenImages/Tamarind-Rice.png'),
+    "navigation": "Tamarind_Rice",
+    "wishlistColor":'white'
+  },
+  {
+    "albumId": 1,
+    "id": 4,
+    "title": "Butter Paneer",
+    "url": require('../../../assets/VegScreenImages/Butter_Paneer.png'),
+    "navigation": "Butter_Paneer",
+    "wishlistColor":'white'
+  },
+  {
+    "albumId": 1,
+    "id": 5,
+    "title": "VGV Vegetable Sandwich",
+    "url": require('../../../assets/VegScreenImages/VGV_Vegetable_Sandwich.png'),
+    "navigation": "VGV_Vegetable_Sandwich",
+    "wishlistColor":'white'
+  },
+  {
+    "albumId": 1,
+    "id": 6,
+    "title": "Pommes Gratin",
+    "url": require('../../../assets/VegScreenImages/Pommes_Gratin.png'),
+    "navigation": "Pommes_Gratin",
+    "wishlistColor":'white'
+  },
+  {
+    "albumId": 1,
+    "id": 7,
+    "title": "Vegetable Pakoda",
+    "url": require('../../../assets/VegScreenImages/Vegetable_Pakoda.png'),
+    "navigation": "Vegetable_Pakoda",
+    "wishlistColor":'white'
+  },
+  {
+    "albumId": 1,
+    "id": 8,
+    "title": "Green Pea Upma",
+    "url": require('../../../assets/VegScreenImages/Green_Pea_Upma.png'),
+    "navigation": "Green_Pea_Upma",
+    "wishlistColor":'white'
+  },
+  {
+    "albumId": 1,
+    "id": 9,
+    "title": "Vegetable Fried Rice",
+    "url": require('../../../assets/VegScreenImages/Vegetable_Fried_Rice.png'),
+    "navigation": "Vegetable_Fried_Rice",
+    "wishlistColor":'white'
+  },
+  {
+    "albumId": 1,
+    "id": 10,
+    "title": "Rajasthani Kadhi With Tofu Palak Pakoda",
+    "url": require('../../../assets/VegScreenImages/Kadhi2.png'),
+    "navigation": "Rajasthani_Kadhi_With_Tofu_Palak_Pakoda",
+    "wishlistColor":'white'
+  },
+  {
+    "albumId": 1,
+    "id": 11,
+    "title": "Wheat Pasta in Mushroom Sauce",
+    "url": require("../../../assets/VegScreenImages/WholeWheatPasta.png"),
+    "navigation": "Pasta_in_Mushroom_Sauce",
+    "wishlistColor":'white'
+  },
+  {
+    "albumId": 1,
+    "id": 12,
+    "title": "Aloo Tamatar Ka Jhol",
+    "url": require('../../../assets/VegScreenImages/Aloo-Tamatar-Ka-Jhol.png'),
+    "navigation": "Aloo_Tamatar_Ka_Jhol",
+    "wishlistColor":'white'
+  },
+  {
+    "albumId": 1,
+    "id": 13,
+    "title": "Tamarind Rice",
+    "url": require('../../../assets/VegScreenImages/Tamarind-Rice.png'),
+    "navigation": "Tamarind_Rice",
+    "wishlistColor":'white'
+  },
+  {
+    "albumId": 1,
+    "id": 14,
+    "title": "Butter Paneer",
+    "url": require('../../../assets/VegScreenImages/Butter_Paneer.png'),
+    "navigation": "Butter_Paneer",
+    "wishlistColor":'white'
+  },
+  {
+    "albumId": 1,
+    "id": 15,
+    "title": "VGV Vegetable Sandwich",
+    "url": require('../../../assets/VegScreenImages/VGV_Vegetable_Sandwich.png'),
+    "navigation": "VGV_Vegetable_Sandwich",
+    "wishlistColor":'white'
+  },
+  {
+    "albumId": 1,
+    "id": 16,
+    "title": "Pommes Gratin",
+    "url": require('../../../assets/VegScreenImages/Pommes_Gratin.png'),
+    "navigation": "Pommes_Gratin",
+    "wishlistColor":'white'
+  },
+  {
+    "albumId": 1,
+    "id": 17,
+    "title": "Vegetable Pakoda",
+    "url": require('../../../assets/VegScreenImages/Vegetable_Pakoda.png'),
+    "navigation": "Vegetable_Pakoda",
+    "wishlistColor":'white'
+  },
+  {
+    "albumId": 1,
+    "id": 18,
+    "title": "Green Pea Upma",
+    "url": require('../../../assets/VegScreenImages/Green_Pea_Upma.png'),
+    "navigation": "Green_Pea_Upma",
+    "wishlistColor":'white'
+  },
+  {
+    "albumId": 1,
+    "id": 19,
+    "title": "Vegetable Fried Rice",
+    "url": require('../../../assets/VegScreenImages/Vegetable_Fried_Rice.png'),
+    "navigation": "Vegetable_Fried_Rice",
+    "wishlistColor":'white'
+  },
+  {
+    "albumId": 1,
+    "id": 20,
+    "title": "Rajasthani Kadhi With Tofu Palak Pakoda",
+    "url": require('../../../assets/VegScreenImages/Kadhi2.png'),
+    "navigation": "Rajasthani_Kadhi_With_Tofu_Palak_Pakoda",
+    "wishlistColor":'white'
+  }
+]
+export default db;
